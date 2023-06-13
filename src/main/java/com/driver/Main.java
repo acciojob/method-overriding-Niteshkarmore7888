ca7f864800dd6c46195aa8d609f7400b64ca6278
Niteshkarmore7888 <niteshkarmo444@gmail.com>
2023-06-13 14:03:09 +0530

    Adding method overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,18 +6,20 @@ public class Main {
             return "Invoking method from class A";
         }
     }
+
     public static class B extends A{
+
         @Override
         public String meth(){
-            return "Method is overridden in Extended class B";
+            return "Method is overridden in Extendend class B";
         }
-
     }
 
     public static void main(String[] args) {
-        B b=new B();
+        B b = new B();
         b.meth();
-        B b1=new B();
+
+        B b1 = new B();
         b1.meth();
     }
 
